71ede2347d6630dc8fb7dfa0189958346f5cf253
Marwin <marwin.menezes@gmail.com>
2024-02-24 11:07:59 +0530

    Update ClaimAssistController.java
    Cors issue fix

--- demo/src/main/java/com/persistent/aicrew/controller/ClaimAssistController.java
+++ demo/src/main/java/com/persistent/aicrew/controller/ClaimAssistController.java
@@ -8,10 +8,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import com.persistent.aicrew.dao.entity.Claim;
 import com.persistent.aicrew.service.ClaimService;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/sample")
 public class ClaimAssistController {
