872a158956bba1c1258950029e11dbfb009a34cf
Marwin Menezes <marwin.menezes3@ibm.com>
2024-02-27 15:21:31 +0530

    updating the data file

--- demo/src/main/resources/data.sql
+++ demo/src/main/resources/data.sql
@@ -1,14 +1,14 @@
 INSERT INTO Claim (id, hospital, patient_name, claim_summary, claim_description) VALUES (1, 'Apollo Hospital', 'Govind Sharma', 'Diagnostic Services', 'X-ray, chest, single view MRI, CT Scan, abdomen and pelvis');
-INSERT INTO Claim (id, hospital, patient_name, claim_summary, claim_description) VALUES (2, 'Tata Memorial Hospital', 'Shweta Singh', 'Diagnostic Services', 'X-ray, chest, single view MRI, CT Scan, abdomen and pelvis');
-INSERT INTO Claim (id, hospital, patient_name, claim_summary, claim_description) VALUES (3, 'Apollo Hospital', 'Sameer Kumar', 'Diagnostic Services', 'X-ray, chest, single view MRI, CT Scan, abdomen and pelvis');
-INSERT INTO Claim (id, hospital, patient_name, claim_summary, claim_description) VALUES (4, 'Nanavati Hospital', 'Raj Khosla', 'Diagnostic Services', 'X-ray, chest, single view MRI, CT Scan, abdomen and pelvis');
-INSERT INTO Claim (id, hospital, patient_name, claim_summary, claim_description) VALUES (5, 'Breach Candy Hospital Trust', 'Manu Sharma', 'Diagnostic Services', 'X-ray, chest, single view MRI, CT Scan, abdomen and pelvis');
+INSERT INTO Claim (id, hospital, patient_name, claim_summary, claim_description) VALUES (2, 'Tata Memorial Hospital', 'Shweta Singh', 'Imaging Studies', 'Ultrasound, abdomen Mammogram, Ecocardiogram, Screening');
+INSERT INTO Claim (id, hospital, patient_name, claim_summary, claim_description) VALUES (3, 'Apollo Hospital', 'Sameer Kumar', 'Laboratory tests', 'Complete Blood count (CBC), comprehensive metabolic panel (CMP)');
+INSERT INTO Claim (id, hospital, patient_name, claim_summary, claim_description) VALUES (4, 'Nanavati Hospital', 'Raj Khosla', 'Procedures', 'Colonoscopy, esophageal dilation, Laparosscopy');
+INSERT INTO Claim (id, hospital, patient_name, claim_summary, claim_description) VALUES (5, 'Breach Candy Hospital Trust', 'Manu Sharma', 'Surgical procedures', 'Appendectomy, Cholecystectomy');
 
-INSERT INTO Underwriting (id, age, gender, bmi, occupation, medical_history, risk_meter, rate_meter, approved) VALUES (1, 30, 'Male', 25.5, 'Engineer', 'No issues', 'low', 2301, true);
-INSERT INTO Underwriting (id, age, gender, bmi, occupation, medical_history, risk_meter, rate_meter, approved) VALUES (2, 25, 'Female', 22.0, 'Doctor', 'No Issues', 'low', 2190, true);
+INSERT INTO Underwriting (id, age, gender, bmi, occupation, medical_history, risk_meter, rate_meter, approved) VALUES (1, 30, 'Male', 25.5, 'Engineer', 'In Good Health', 'low', 2301, true);
+INSERT INTO Underwriting (id, age, gender, bmi, occupation, medical_history, risk_meter, rate_meter, approved) VALUES (2, 25, 'Female', 22.0, 'Doctor', 'In Good Health', 'low', 2190, true);
 INSERT INTO Underwriting (id, age, gender, bmi, occupation, medical_history, risk_meter, rate_meter, approved) VALUES (3, 35, 'Male', 28.3, 'Teacher', 'Hypertension', 'high', 4000, false);
-INSERT INTO Underwriting (id, age, gender, bmi, occupation, medical_history, risk_meter, rate_meter, approved) VALUES (4, 28, 'Female', 26.8, 'Software Developer', 'No issues', 'medium', 3120, true);
-INSERT INTO Underwriting (id, age, gender, bmi, occupation, medical_history, risk_meter, rate_meter, approved) VALUES (5, 40, 'Male', 30.1, 'Nurse', 'Diabetes', 'high', 3330, false);
+INSERT INTO Underwriting (id, age, gender, bmi, occupation, medical_history, risk_meter, rate_meter, approved) VALUES (4, 28, 'Female', 26.8, 'Software Developer', 'Respiratory problems', 'medium', 3120, true);
+INSERT INTO Underwriting (id, age, gender, bmi, occupation, medical_history, risk_meter, rate_meter, approved) VALUES (5, 40, 'Male', 30.1, 'Nurse', 'Diabetes, Cardiovascular disease', 'high', 3330, false);
 
 Insert INTO Bill (claim_id, bill_id, bill_date, bill_description, bill_amount) values (1, 1, '2023-01-02', 'Consultation charges', 200 );
 Insert INTO Bill (claim_id, bill_id, bill_date, bill_description, bill_amount) values (1, 2, '2023-01-02', 'ECG charges', 200 );
